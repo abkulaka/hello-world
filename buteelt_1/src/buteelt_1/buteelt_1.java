446153f22348581ffbaec3e2c088b6f69aea2ae6
User <User@172.16.153.88>
2024-01-30 15:49:12 +0800

    k

---
+++ buteelt_1/src/buteelt_1/buteelt_1.java
@@ -0,0 +1,19 @@
+package buteelt_1;
+import java.util.Scanner;
+public class buteelt_1 {
+ private static final double PI = 3.14;
+ private final double radius;
+ public buteelt_1(double radius) {
+ this.radius = radius;
+ }
+ public static void main(String[] args) {
+ System.out.println("------------------------------------------------------------");
+ System.out.println("CircleCalc v1.0");
+ System.out.println();
+ System.out.println("Calculates and prints information for a user-supplied radius");
+ System.out.println("------------------------------------------------------------");
+ System.out.print("Enter the circle's radius: ");
+ Scanner in = new Scanner(System.in);
+ buteelt_1 c = new buteelt_1(in.nextFloat()) ;
+ }
+}
