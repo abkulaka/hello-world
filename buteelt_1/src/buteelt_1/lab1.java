133a2c2c6be0a6d8c097948cb7ef4dd00d928949
User <User@172.16.152.30>
2024-02-27 15:11:40 +0800

    adada

--- buteelt_1/src/buteelt_1/lab1.java
+++ buteelt_1/src/buteelt_1/lab1.java
@@ -1,181 +1,37 @@
 package buteelt_1;
 
-class Node {
-    int data;
-    Node prev;
-    Node next;
-
-    public Node(int data) {
-        this.data = data;
-        this.prev = null;
-        this.next = null;
-    }
-}
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
 
 public class lab1 {
-
-    Node head;
-    Node tail;
-
-    public lab1() {
-        this.head = null;
-        this.tail = null;
-    }
-
-    public void addNode(int data) {
-        Node newNode = new Node(data);
-
-        if (head == null) {
-            head = newNode;
-            tail = newNode;
-        } else {
-            tail.next = newNode;
-            newNode.prev = tail;
-            tail = newNode;
-        }
-    }
-
-    public void addNodeToFront(int data) {
-        Node newNode = new Node(data);
-        if (head == null) {
-            head = newNode;
-            tail = newNode;
-        } else {
-            head.prev = newNode;
-            newNode.next = head;
-            head = newNode;
-        }
-    }
-
-    public void addNodeAfter(int key, int data) {
-        Node newNode = new Node(data);
-        Node current = head;
-        while (current != null) {
-            if (current.data == key) {
-                newNode.next = current.next;
-                if (current.next != null)
-                    current.next.prev = newNode;
-                current.next = newNode;
-                newNode.prev = current;
-                if (current == tail)
-                    tail = newNode;
-                break;
-            }
-            current = current.next;
-        }
-    }
-
-    public void insertAfter(Node prevNode, int data) {
-        if (prevNode == null) {
-            System.out.println("Previous node cannot be null");
-            return;
-        }
-
-        Node newNode = new Node(data);
-        newNode.next = prevNode.next;
-        if (prevNode.next != null)
-            prevNode.next.prev = newNode;
-        prevNode.next = newNode;
-        newNode.prev = prevNode;
-    }
-
-    public void deleteNode(int key) {
-        Node current = head;
-        while (current != null) {
-            if (current.data == key) {
-                if (current == head) {
-                    head = head.next;
-                    if (head != null)
-                        head.prev = null;
-                    if (head == null)
-                        tail = null;
-                } else if (current == tail) {
-                    tail = tail.prev;
-                    tail.next = null;
-                } else {
-                    current.prev.next = current.next;
-                    current.next.prev = current.prev;
-                }
-                break;
-            }
-            current = current.next;
-        }
-    }
-
-    public int getSize() {
-        int size = 0;
-        Node current = head;
-        while (current != null) {
-            size++;
-            current = current.next;
-        }
-        return size;
-    }
-
-    public boolean isEmpty() {
-        return head == null;
-    }
-
-    public void reverseList() {
-        Node temp = null;
-        Node current = head;
-        while (current != null) {
-            temp = current.prev;
-            current.prev = current.next;
-            current.next = temp;
-            current = current.prev;
-        }
-        if (temp != null) {
-            head = temp.prev;
-        }
-    }
-
-    public void displayList() {
-        Node current = head;
-        System.out.print("Doubly Linked List: ");
-        while (current != null) {
-            System.out.print(current.data + " ");
-            current = current.next;
-        }
-        System.out.println();
-    }
-
     public static void main(String[] args) {
-        lab1 doublyLinkedList = new lab1();
+        // 1. List үүсгэх
+        List<String> myList = new ArrayList<>();
 
-        doublyLinkedList.addNode(5);
-        doublyLinkedList.addNode(7);
-        doublyLinkedList.addNode(18);
+        // 2. Элемент нэмэх
+        myList.add("Java");
+        myList.add("Python");
+        myList.add("C++");
+        myList.add("JavaScript");
 
-        doublyLinkedList.displayList();
+        // 3. Элемент хасах
+        myList.remove("C++");
 
-        // 1 утга нэмэх
-        doublyLinkedList.addNodeToFront(10);
-        doublyLinkedList.displayList();
+        // 4. Элемент хайх
+        boolean containsJava = myList.contains("Java");
+        System.out.println("Java хэл байгаа эсэх'? " + containsJava);
 
-        // зангилаа нэмэх
-        doublyLinkedList.addNodeAfter(10, 17);
-        doublyLinkedList.displayList();
+        // 5. Элементийг олох
+        String language = myList.get(0);
+        System.out.println("Анхны ашигласан хэл бол: " + language);
 
-        // зангилаа устгах
-        doublyLinkedList.deleteNode(18);
-        doublyLinkedList.displayList();
+        // 6. List-ийн хэмжээг олгох
+        int size = myList.size();
+        System.out.println("List-ны хэмжээ : " + size);
 
-        // insert node after specific node
-        Node prevNode = doublyLinkedList.head.next; // Insert after node with value 7
-        doublyLinkedList.insertAfter(prevNode, 25);
-        doublyLinkedList.displayList();
-
-        // дараалал доторх нийт тоон хэмжээ
-        System.out.println("Дарааллын хэмжээ: " + doublyLinkedList.getSize());
-
-        // Хоосон эсэхийг шалгах
-        System.out.println("Дараалал хоосон эсэх? " + doublyLinkedList.isEmpty());
-
-        // Урвуу дараалал
-        doublyLinkedList.reverseList();
-        doublyLinkedList.displayList();
+        // 7. List-ийг эрэмбэлэх
+        Collections.sort(myList);
+        System.out.println("Доторх хэлнүүд: " + myList);
     }
 }
-
-
